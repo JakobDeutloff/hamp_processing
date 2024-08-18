3e51e64f3015b79f1a9ef870920f54d0c156283c
clara.bayley <yoctoyotta1024@yahoo.com>
2024-08-18 21:02:33 +0200

    feat: new file for earthcare specific quicklooks

---
+++ scripts/earthcare_hamp_quicklooks.py
@@ -0,0 +1,73 @@
+# %%
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import pandas as pd
+from src import plot_quicklooks as plotql
+from src import load_data_functions as loadfuncs
+from src import helper_functions as helpfuncs
+import yaml
+
+# %%
+### ----------- USER PARAMETERS YOU MUST SET IN CONFIG.YAML ----------- ###
+# Load configuration from YAML file
+with open("config.yaml", "r") as file:
+    config = yaml.safe_load(file)
+
+# Extract parameters from the configuration
+flight = config["flight"]
+date = config["date"]
+flightletter = config["flightletter"]
+is_planet = config["is_planet"]
+
+# Format paths using the extracted parameters
+path_bahamas = config["paths"]["bahamas"].format(date=date, flightletter=flightletter)
+path_radiometer = config["paths"]["radiometer"].format(
+    date=date, flightletter=flightletter
+)
+path_radar = config["paths"]["radar"].format(flight=flight)
+savedir = config["savedir"].format(flight=flight)
+
+### ---------------------------------------------------- ###
+
+# %% create HAMP post-processed data
+hampdata = loadfuncs.do_post_processing(
+    path_bahamas, path_radar, path_radiometer, date[2:], is_planet=is_planet
+)
+
+# %% get earthcare track forecasts
+track = helpfuncs.get_earthcare_track(config["date"])
+
+# %% find time when earthcare crosses halo
+ec_under_time = helpfuncs.find_crossing_time(track, hampdata.flightdata)
+
+# %% produce ec_under single quicklook
+windowsize = pd.Timedelta("30m")
+starttime, endtime = ec_under_time - windowsize / 2, ec_under_time + windowsize / 2
+is_savefig = True
+savename = f"{savedir}/ec_under_{flight}.png"
+dpi = 500
+plotql.hamp_timeslice_quicklook(
+    hampdata,
+    timeframe=slice(starttime, endtime),
+    flight=flight,
+    ec_under_time=ec_under_time,
+    figsize=(18, 18),
+    savefigparams=[is_savefig, savename, dpi],
+)
+
+# %% produce radar-only single quicklook between startime and endtime
+windowsize = pd.Timedelta("30m")
+starttime, endtime = ec_under_time - windowsize / 2, ec_under_time + windowsize / 2
+is_savefig = True
+savename = f"{savedir}/radar_timesliceql_{flight}.png"
+dpi = 500
+plotql.radar_quicklook(
+    hampdata,
+    timeframe=slice(starttime, endtime),
+    flight=flight,
+    figsize=(9, 5),
+    savefigparams=[is_savefig, savename, dpi],
+)
