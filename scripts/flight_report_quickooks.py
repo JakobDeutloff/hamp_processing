ab430335f6c90cb695d44c94a23c9ffeac05f11f
clara.bayley <yoctoyotta1024@yahoo.com>
2024-08-19 20:10:47 +0200

    fix: make figure wider

--- scripts/flight_report_quickooks.py
+++ scripts/flight_report_quickooks.py
@@ -51,7 +51,7 @@ plotql.hamp_timeslice_quicklook(
     timeframe=slice(starttime, endtime),
     flight=flight,
     ec_under_time=ec_under_time,
-    figsize=(24, 20),
+    figsize=(28, 20),
     savefigparams=[is_savefig, savename, dpi],
 )
 
@@ -70,7 +70,7 @@ plotql.hamp_timeslice_quicklook(
     timeframe=slice(ec_starttime, ec_endtime),
     flight=flight,
     ec_under_time=ec_under_time,
-    figsize=(24, 20),
+    figsize=(28, 20),
     savefigparams=[is_savefig, savename, dpi],
 )
 
