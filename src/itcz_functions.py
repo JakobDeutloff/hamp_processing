b00e49303d76d81907f04686a6f7075037ede3b6
clara.bayley <yoctoyotta1024@yahoo.com>
2024-08-22 18:20:23 +0200

    feat: new file for itcz related functions

--- src/itcz_functions.py
+++ src/itcz_functions.py
@@ -8,7 +8,6 @@ def identify_itcz_crossings(cwv, thresholds=[45, 50]):
     outside_itcz_threshold < CWV <= inside_itcz_threshold = 1;
     CWV > inside_itcz_threshold = or 2.
     """
-    # create mask
     itcz_mask = np.where(cwv < thresholds[0], 0, 1)
     itcz_mask = np.where(cwv > thresholds[1], 2, itcz_mask)
     itcz_mask = np.where(np.isnan(cwv), np.nan, itcz_mask)
