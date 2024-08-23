8035e04930bcae3395d90f9d3f09f819e720df82
clara.bayley <yoctoyotta1024@yahoo.com>
2024-08-23 16:20:11 +0200

    refactor: move plotting into function

--- scripts/itcz_plots.py
+++ scripts/itcz_plots.py
@@ -11,6 +11,74 @@ from src import plot_functions as plotfuncs
 from src import itcz_functions as itczfuncs
 from src.plot_quicklooks import save_figure
 
+
+def plot_itcz_masked_radar_timeseries_and_composite(
+    hampdata,
+    itcz_mask_signal,
+    figsize=(12, 9),
+    savefigparams=[],
+):
+    fig, axs = plt.subplots(
+        nrows=3,
+        ncols=2,
+        figsize=figsize,
+        width_ratios=[18, 7],
+        sharey="row",
+        sharex="col",
+    )
+
+    time_radar = hampdata.radar.time
+    height_km = hampdata.radar.height / 1e3  # [km]
+    signal = plotfuncs.filter_radar_signal(hampdata.radar.dBZg, threshold=-30)  # [dBZ]
+
+    nrep = len(height_km)
+    itcz_mask_signal = np.repeat(itcz_mask_signal, nrep)
+    itcz_mask_signal = np.reshape(itcz_mask_signal.values, [len(time_radar), nrep])
+
+    mask_values = {0: "Outside", 1: "Transition", 2: "Inside"}
+
+    for a in mask_values.keys():
+        ax0, ax1 = axs[a, 0], axs[a, 1]
+
+        signal_plt = np.where(itcz_mask_signal == a, signal, np.nan)
+
+        ax1.set_title(f"{mask_values[a]}", loc="left")
+        cax = plotfuncs.plot_radardata_timeseries(
+            time_radar, height_km, signal_plt.T, fig, ax0
+        )[1]
+        ax0.set_xlabel("")
+
+        signal_range = [-30, 30]
+        height_range = [0, np.nanmax(height_km)]
+        signal_bins = 60
+        height_bins = 100
+        cmap = plotfuncs.get_greys_histogram_colourmap(signal_bins)
+        plotfuncs.plot_radardata_histogram(
+            time_radar,
+            height_km,
+            signal_plt,
+            ax1,
+            signal_range,
+            height_range,
+            height_bins,
+            signal_bins,
+            cmap,
+        )
+        axs[a, 1].set_ylabel("")
+
+    timeframe = slice(hampdata.radar.time[0], hampdata.radar.time[-1])
+    plotfuncs.add_lat_lon_axes(hampdata, timeframe, axs[2, 0])
+    axs[2, 0].set_xlabel("UTC")
+    axs[2, 1].set_xlabel("Z /dBZe")
+    plotfuncs.beautify_axes(axs.flatten())
+    plotfuncs.beautify_colorbar_axes(cax)
+
+    fig.tight_layout()
+
+    if savefigparams != []:
+        save_figure(fig, savefigparams=[savefig_format, savename, dpi])
+
+
 # # %%
 # ### -------- USER PARAMETERS YOU MUST SET IN CONFIG.YAML -------- ###
 # import yaml
@@ -98,57 +166,6 @@ save_figure(fig, savefigparams=[savefig_format, savename, dpi])
 savefig_format = "png"
 savename = path_saveplts / "itcz_mask_radar_composite.png"
 dpi = 64
-
-fig, axs = plt.subplots(
-    nrows=3, ncols=2, figsize=(12, 9), width_ratios=[18, 7], sharey="row", sharex="col"
+plot_itcz_masked_radar_timeseries_and_composite(
+    hampdata, itcz_mask_2, savefigparams=[savefig_format, savename, dpi]
 )
-
-time_radar = hampdata.radar.time
-height_km = hampdata.radar.height / 1e3  # [km]
-signal = plotfuncs.filter_radar_signal(hampdata.radar.dBZg, threshold=-30)  # [dBZ]
-
-nrep = len(height_km)
-itcz_mask_signal = np.repeat(itcz_mask_2, nrep)
-itcz_mask_signal = np.reshape(itcz_mask_signal.values, [len(time_radar), nrep])
-
-mask_values = {0: "Outside", 1: "Transition", 2: "Inside"}
-
-for a in mask_values.keys():
-    ax0, ax1 = axs[a, 0], axs[a, 1]
-
-    signal_plt = np.where(itcz_mask_signal == a, signal, np.nan)
-
-    ax1.set_title(f"{mask_values[a]}", loc="left")
-    cax = plotfuncs.plot_radardata_timeseries(
-        time_radar, height_km, signal_plt.T, fig, ax0
-    )[1]
-    ax0.set_xlabel("")
-
-    signal_range = [-30, 30]
-    height_range = [0, np.nanmax(height_km)]
-    signal_bins = 60
-    height_bins = 100
-    cmap = plotfuncs.get_greys_histogram_colourmap(signal_bins)
-    plotfuncs.plot_radardata_histogram(
-        time_radar,
-        height_km,
-        signal_plt,
-        ax1,
-        signal_range,
-        height_range,
-        height_bins,
-        signal_bins,
-        cmap,
-    )
-    axs[a, 1].set_ylabel("")
-
-timeframe = slice(hampdata.radar.time[0], hampdata.radar.time[-1])
-plotfuncs.add_lat_lon_axes(hampdata, timeframe, axs[2, 0])
-axs[2, 0].set_xlabel("UTC")
-axs[2, 1].set_xlabel("Z /dBZe")
-plotfuncs.beautify_axes(axs.flatten())
-plotfuncs.beautify_colorbar_axes(cax)
-
-fig.tight_layout()
-
-save_figure(fig, savefigparams=[savefig_format, savename, dpi])
