bdb1daa01a7fd201903517c994222192ada8cc34
clara.bayley <yoctoyotta1024@yahoo.com>
2024-08-31 13:47:02 +0200

    refactor: add latmax to func

--- scripts/dropsonde_winds.py
+++ scripts/dropsonde_winds.py
@@ -12,6 +12,7 @@ from src import load_data_functions as loadfuncs
 from src import plot_functions as plotfuncs
 from src import dropsonde_wind_analyses as dropfuncs
 from src.plot_quicklooks import save_figure
+from matplotlib.cm import ScalarMappable
 
 
 # %% helper function definitions
@@ -101,6 +102,61 @@ def mean_winds_seperate_east_north_scatter(
     return fig, axes
 
 
+def plot_allflights_wind_vertical_profile(
+    ds_full,
+    dates,
+    calc_verticaldata,
+    latmax,
+    cmap,
+    vmin,
+    vmax,
+    hmin,
+    hmax,
+    xlabel,
+    figtitle,
+):
+    fig, axes = plt.subplots(
+        nrows=1, ncols=7, figsize=(21, 9), width_ratios=[1] * 6 + [1 / 27]
+    )
+
+    for d, date in enumerate(dates):
+        ds_dropsonde = loadfuncs.load_dropsonde_data_for_date(ds_full, date)
+        ds_dropsonde = ds_dropsonde.where(ds_dropsonde.lat < latmax, drop=True)
+        height = np.tile(ds_dropsonde.gpsalt / 1000, ds_dropsonde.sonde_id.size)  # [km]
+        colorby = ds_dropsonde["lat"].values.flatten()
+
+        ax, norm1, cmap1 = dropfuncs.plot_coloured_dropsonde_vertical_profile(
+            axes[d],
+            calc_verticaldata(ds_dropsonde),
+            height,
+            vmin,
+            vmax,
+            colorby,
+            cmap,
+            axtitle=None,
+            xlabel=xlabel,
+        )
+        ax.set_title(f"HALO-{date}a", fontsize=15)
+        ax.set_ylim(hmin, hmax)
+
+    for ax in axes[1:6]:
+        ax.sharey(axes[0])
+        ax.sharex(axes[0])
+        ax.set_ylabel("")
+
+    fig.suptitle(figtitle, fontsize=15)
+    cax = fig.colorbar(
+        ScalarMappable(norm=norm1, cmap=cmap1),
+        cax=axes[6],
+        label="latitude /$\u00B0$",
+        shrink=0.8,
+    )
+    plotfuncs.beautify_axes(axes)
+    plotfuncs.beautify_colorbar_axes(cax)
+
+    return fig, axes
+
+
 # %% load config and create HAMP post-processed data
 ### -------- USER PARAMETERS YOU MUST SET IN CONFIG.YAML -------- ###
 configfile = sys.argv[1]
@@ -108,60 +164,60 @@ cfg = rwfuncs.extract_config_params(configfile)
 path_saveplts = cfg["path_saveplts"]
 flightname = cfg["flightname"]
 ### ------------------------------------------------------------- ###
-ds = loadfuncs.load_dropsonde_data_for_date(cfg["path_dropsonde_level3"], cfg["date"])
-# hampdata = loadfuncs.do_post_processing(
-#     cfg["path_bahamas"],
-#     cfg["path_radar"],
-#     cfg["path_radiometer"],
-#     cfg["radiometer_date"],
-#     is_planet=cfg["is_planet"],
-#     do_radar=True,
-#     do_183=True,
-#     do_11990=True,
-#     do_kv=True,
-#     do_cwv=True,
+# ds = loadfuncs.load_dropsonde_data_for_date(cfg["path_dropsonde_level3"], cfg["date"])
+# # hampdata = loadfuncs.do_post_processing(
+# #     cfg["path_bahamas"],
+# #     cfg["path_radar"],
+# #     cfg["path_radiometer"],
+# #     cfg["radiometer_date"],
+# #     is_planet=cfg["is_planet"],
+# #     do_radar=True,
+# #     do_183=True,
+# #     do_11990=True,
+# #     do_kv=True,
+# #     do_cwv=True,
+# # )
+
+# # %% Plot Vertical Wind Profiles Coloured by Latitude
+# latmax = 15
+# ds_dropsonde = ds.where(ds.lat < latmax, drop=True)
+
+# fig, axes = dropfuncs.plot_dropsonde_wind_vertical_profiles(
+#     ds_dropsonde,
+#     "lat",
+#     figsize=(21, 12),
+#     cmap="plasma",
+#     cbarlab="latitude /$\u00B0$",
 # )
+# savefig_format = "png"
+# savename = (
+#     path_saveplts / f"dropsonde_vertical_wind_profiles_{flightname}_colorlatitutde.png"
+# )
+# dpi = 64
+# save_figure(fig, savefigparams=[savefig_format, savename, dpi])
 
-# %% Plot Vertical Wind Profiles Coloured by Latitude
-latmax = 15
-ds_dropsonde = ds.where(ds.lat < latmax, drop=True)
+# # %% Plot Vertical Wind Profiles Coloured by RelH
+# fig, axes = dropfuncs.plot_dropsonde_wind_vertical_profiles(
+#     ds_dropsonde,
+#     "rh",
+#     figsize=(21, 12),
+#     cmap="Blues",
+#     cbarlab="relative humidity Kg/Kg",
+# )
+# savefig_format = "png"
+# savename = (
+#     path_saveplts / f"dropsonde_vertical_wind_profiles_{flightname}_colorrelh.png"
+# )
+# dpi = 64
+# save_figure(fig, savefigparams=[savefig_format, savename, dpi])
 
-fig, axes = dropfuncs.plot_dropsonde_wind_vertical_profiles(
-    ds_dropsonde,
-    "lat",
-    figsize=(21, 12),
-    cmap="plasma",
-    cbarlab="latitude /$\u00B0$",
-)
-savefig_format = "png"
-savename = (
-    path_saveplts / f"dropsonde_vertical_wind_profiles_{flightname}_colorlatitutde.png"
-)
-dpi = 64
-save_figure(fig, savefigparams=[savefig_format, savename, dpi])
-
-# %% Plot Vertical Wind Profiles Coloured by RelH
-fig, axes = dropfuncs.plot_dropsonde_wind_vertical_profiles(
-    ds_dropsonde,
-    "rh",
-    figsize=(21, 12),
-    cmap="Blues",
-    cbarlab="relative humidity Kg/Kg",
-)
-savefig_format = "png"
-savename = (
-    path_saveplts / f"dropsonde_vertical_wind_profiles_{flightname}_colorrelh.png"
-)
-dpi = 64
-save_figure(fig, savefigparams=[savefig_format, savename, dpi])
-
-# %% Plot Mean Winds Between Heights
-heights = [0, 2000, 4000, 6000, 8000, 10000, 12000]
-fig, axes = mean_wind_quivers_plot(ds_dropsonde, heights, figsize=(15, 9))
-savefig_format = "png"
-savename = path_saveplts / f"dropsonde_wind_slices_{flightname}_colorrelh.png"
-dpi = 64
-save_figure(fig, savefigparams=[savefig_format, savename, dpi])
+# # %% Plot Mean Winds Between Heights
+# heights = [0, 2000, 4000, 6000, 8000, 10000, 12000]
+# fig, axes = mean_wind_quivers_plot(ds_dropsonde, heights, figsize=(15, 9))
+# savefig_format = "png"
+# savename = path_saveplts / f"dropsonde_wind_slices_{flightname}_colorrelh.png"
+# dpi = 64
+# save_figure(fig, savefigparams=[savefig_format, savename, dpi])
 
 # # %% Plot Mean Winds Between Height with scatter for components
 # for h in range(1, len(heights)):
@@ -177,3 +233,37 @@ save_figure(fig, savefigparams=[savefig_format, savename, dpi])
 #         latmax=20,
 #     )
 # plt.show()
+
+# %% Plot Vertical Wind Profile for ALl Flights Coloured by Latitude
+latmax = 15
+dates = ["20240811", "20240813", "20240816", "20240818", "20240821", "20240822"]
+vmin, vmax = 5, 12
+hmin, hmax = -0.5, 15
+
+ds_full = loadfuncs.load_dropsonde_data(cfg["path_dropsonde_level3"])
+figtitle = "Eastward Wind Component"
+xlabel = "u /m s$^{-1}$"
+cmap = "plasma"
+
+
+def calc_verticaldata(ds_dropsonde):
+    return ds_dropsonde.u.values.flatten()
+
+
+fig, axes = plot_allflights_wind_vertical_profile(
+    ds_full,
+    dates,
+    calc_verticaldata,
+    latmax,
+    cmap,
+    vmin,
+    vmax,
+    hmin,
+    hmax,
+    xlabel,
+    figtitle,
+)
+savefig_format = "png"
+savename = path_saveplts / "allflights_vertical_eastward_profile_colorlatitutde.png"
+dpi = 64
+save_figure(fig, savefigparams=[savefig_format, savename, dpi])
